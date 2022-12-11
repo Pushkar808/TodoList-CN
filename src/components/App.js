895891888fde71bd7c5b524cdcfccf13b6ef98f2
Pushkar gupta <pushkargupta808@gmail.com>
2022-12-11 23:52:44 +0530

    added todolist items

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import '../App.css';
+import Userselector from './Userselector';
+function App() {
+  return (
+    <div className="App">
+      <Userselector/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Userselector.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react'
+import '../App.css'
+const Userselector = () => {
+    const [data, setData] = useState()
+    //use Effect hook to fetch data for the first time 
+    useEffect(() => {
+        console.log("OK")
+        fetchAPI(1)//fetch API for user 1 on starting
+    }, [])
+
+    const fetchAPI = (userId) => {
+        return fetch(`https://jsonplaceholder.typicode.com/todos/?userId=${userId}`)//fetch all the task for user id 1
+            .then((response) => response.json())
+            .then((json) => {
+                setData(json)//setting the todo list data to the state
+            });
+    }
+    return (
+        <>
+            <div className='container'>
+                <div id="selectorContainer">
+                    <select id="userSelector" onChange={(e) => { fetchAPI(e.target.value) }} >
+                        {/* {fetching value on change of the list} */}
+                        <option value={1} onClick={() => { fetchAPI(1) }}>user1</option>
+                        <option value={2} onClick={() => { fetchAPI(2) }}>user2</option>
+                        <option value={3} onClick={() => { fetchAPI(3) }}>user3</option>
+
+                    </select>
+                </div>
+                {data && data.length > 0 && data.map((todoItem, index) => (
+                    <li key={todoItem.id}>
+                        <div>{todoItem.title}
+                            {todoItem.completed ?
+                                <i class="fa-solid fa-square-check"></i>
+                                :
+                                <i class="fa-solid fa-square-xmark"></i>
+                            }
+                        </div>
+                    </li>
+
+                ))}
+            </div>
+        </>
+    )
+}
+
+export default Userselector
