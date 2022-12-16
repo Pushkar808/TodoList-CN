9d38fd2123b60eb91c897836eb070e5285eab56f
Pushkar gupta <pushkargupta808@gmail.com>
2022-12-16 22:21:03 +0530

    added delete request

--- src/components/Addlist.js
+++ src/components/Addlist.js
@@ -2,12 +2,13 @@ import React, { useState } from 'react'
 
 export default function Addlist(props) {
 
+  //title/body:to store the title/body for the create/update
   const [title, settitle] = useState("");
   const [body, setbody] = useState("");
 
 
   //function to show a small pop up like message
-  function showToast(title, body,note) {
+  function showToast(title, body, note) {
     document.getElementById('response').innerHTML = `
     <div id="toast"> 
     <p>Following data has been <b>${note}</b> by the user ${props.userId}</p>
@@ -33,7 +34,7 @@ export default function Addlist(props) {
     })
       .then((response) => response.json())
       .then((json) => {
-        showToast(json.title, json.body,"added");
+        showToast(json.title, json.body, "added");
       });
   }
   function updateResponse() {
@@ -50,16 +51,33 @@ export default function Addlist(props) {
         'Content-type': 'application/json; charset=UTF-8',
       },
     })
-      .then((response) => response.json())
+      .then((response) => {
+        response.json();
+        console.log(response.status)
+      })
       .then((json) => {
-        showToast(json.title, json.body,"updated");
+        showToast(json.title, json.body, "updated");
       });
   }
+  function deleteResponse() {
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+      method: 'DELETE',
+    })
+      .then((response) => {
+        if (response.status == 200) {//if we get reponse 200 i.e we have executed delete request
+          document.getElementById('response').innerHTML = `
+          <div id="toast"> 
+          <p>Data has been <b>deleted</b> by the user ${props.userId}</p>
+          </div>
+          `
+          setTimeout(() => document.getElementById('response').innerHTML = "", 2000);
+        }
+      })
+  }
   return (
-
     <div>
       <div id="response">
-
+      {/* Toast message will be seen here */}
       </div>
       <div className='form-container'>
         <div className='input-field'>
@@ -75,6 +93,10 @@ export default function Addlist(props) {
         </div>
         <div className='input-field'>
           <button onClick={updateResponse}>Update Response</button>
+        </div>
+        
+        <div className='input-field'>
+          <button onClick={deleteResponse}>Delete Response</button>
         </div>
       </div>
     </div>
